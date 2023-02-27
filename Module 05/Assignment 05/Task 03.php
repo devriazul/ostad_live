067822de77ee3060e42691281ae63f4d4f971e92
Riazul Islam <riazul56@gmail.com>
2023-02-27 18:01:34 +0600

    Assignment finalize

--- Module 05/Assignment 05/Task 03.php
+++ Module 05/Assignment 05/Task 03.php
@@ -29,17 +29,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $person->setName($name);
   $person->setEmail($email);
 
-  echo "Name: " . $person->getName() . "<br>";
-  echo "Email: " . $person->getEmail();
+  echo "<h2>" . "Name: " . $person->getName() . "<br>" . "</h2>";
+  echo "<h2>" . "Email: " . $person->getEmail() . "</h2>";
 }
 
 ?>
-<form action="" method="POST">
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name"><br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Assignment Task</title>
+</head>
+<body>
+<div class="container pt-3 card shadow mt-5 d-block">
+<form class="p-3" action="" method="POST">
+  <label class="form-label" for="name">Name:</label>
+  <input class="form-items" type="text" id="name" name="name"><br><br>
   
-  <label for="email">Email:</label>
-  <input type="email" id="email" name="email"><br><br>
+  <label class="form-label" for="email">Email:</label>
+  <input class="form-items" type="email" id="email" name="email"><br><br>
 
   <input type="submit" value="Submit">
-</form>  
+</form> 
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html> 
+
