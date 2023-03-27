000d9caef794b79ae634825facff20f9b8630204
Riazul Islam <riazul56@gmail.com>
2023-03-27 22:52:41 +0600

    Update

--- Module 08/Assignment 08/login.php
+++ Module 08/Assignment 08/login.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<title>Login Form</title>
 </head>
 <body>
 	<h1>Login Form</h1>
-	<form method="POST" action="login.php">
+	<form class="form" method="POST" action="login.php">
 		<label for="email">Email address:</label>
-		<input type="email" name="email" id="email" required><br><br>
+		<input class="form-control-text" type="email" name="email" id="email" required><br><br>
 
 		<label for="password">Password:</label>
 		<input type="password" name="password" id="password" required><br><br>
