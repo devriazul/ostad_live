59ab6a3e6f715f828afb1aa01956f77fd5dfd8bb
Riazul Islam <riazul56@gmail.com>
2023-06-04 22:49:13 +0600

    Assignment

---
+++ Module 16/MigrationAssignment/database/migrations/2023_06_04_164327_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->text('description');
+            $table->timestamps();
+        });
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('quantity')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
