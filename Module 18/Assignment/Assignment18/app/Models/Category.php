77d7984961700b52127e4ff8ad25f73c54376385
devriazul <riazul56@gmail.com>
2023-07-02 17:21:52 +0600

    Last update

---
+++ Module 18/Assignment/Assignment18/app/Models/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
+
---
+++ Module 18/Assignment/Assignment18/database/migrations/2023_06_25_160340_add_category_id_to_posts_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdToPostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained('categories');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+    }
+};
